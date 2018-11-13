39e3117ebd79b3dc840c0438612cc0ad2b6ff488
Paloma Bispo <palomabispo@alu.ufc.br>
2018-11-13 10:40:57 -0300

    setting CloudKit

---
+++ Pomar/Controller/Study Groups/StudyGroupsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudyGroupsViewController.swift
+//  Pomar
+//
+//  Created by Paloma Bispo on 13/11/18.
+//  Copyright © 2018 Paloma Bispo. All rights reserved.
+//
+
+import UIKit
+
+class StudyGroupsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
