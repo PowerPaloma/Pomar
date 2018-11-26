4cb390e9986f9ddd0d0a601943e5984158cbabcd
Mateus Rodrigues <mateus.fr26@gmail.com>
2018-11-26 08:36:27 -0300

    Modify User Model

--- Pomar/Model/User.swift
+++ Pomar/Model/User.swift
@@ -8,14 +8,52 @@
 
 import Foundation
 import CloudKit
+import UIKit
 
 class User {
     
-    var name: String?
-    var skills: [String]?
+    var id: CKRecord.ID?
+    var name: String
+    var token: String
+    var profileImage: UIImage?
+    var groups: [CKRecord.Reference]?
+    
+    init(name: String, image: UIImage, token: String) {
+        self.name = name
+        self.token = token
+        self.profileImage = image
+    }
     
     init(record: CKRecord) {
-        self.name = record["name"] as? String
-        self.skills = record["skills"] as? [String]
+        self.id = record.recordID
+        self.name = record["name"] as! String
+        self.token = record["token"] as! String
+        self.groups = (record["groups"] as? [CKRecord.Reference])
+        if let asset = record["profileImage"] as? CKAsset {
+            do {
+                let data = try Data(contentsOf: asset.fileURL)
+                self.profileImage = UIImage(data: data)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+}
+
+extension CKRecord {
+    convenience init(user: User) {
+        self.init(recordType: "User")
+        self["name"] = user.name
+        
+        let data = user.profileImage?.jpegData(compressionQuality: 0.1)
+        let url = NSURL.fileURL(withPath: NSTemporaryDirectory()).appendingPathComponent(NSUUID().uuidString+".png")
+        do {
+            try data!.write(to: url)
+        } catch let e as NSError {
+            self["profileImage"] = CKAsset(fileURL: url)
+            print("Error! \(e)")
+        }
+        self["token"] = user.token
     }
 }
