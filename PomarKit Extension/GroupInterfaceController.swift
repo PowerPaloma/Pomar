9d22955d41d943e4bd7c433694d340df612400b8
Thalia Freitas <thaliafreitas@10-45-48-196.0200.ifce.br.iosda.org>
2018-12-06 15:54:55 -0300

    show groups

---
+++ PomarKit Extension/GroupInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  GroupInterfaceController.swift
+//  PomarKit Extension
+//
+//  Created by Thalia Freitas on 05/12/18.
+//  Copyright © 2018 Paloma Bispo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GroupInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ PomarKit Extension/RowCellController.swift
@@ -0,0 +1,13 @@
+//
+//  RowCellController.swift
+//  PomarKit Extension
+//
+//  Created by Thalia Freitas on 06/12/18.
+//  Copyright © 2018 Paloma Bispo. All rights reserved.
+//
+
+import WatchKit
+
+class RowCellController: NSObject {
+
+}
