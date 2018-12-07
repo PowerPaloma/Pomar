dc5fa845aef669a5d1fdc2e63a13dec50fbd1e30
Thalia Freitas <thaliafreitas@10-45-48-196.0200.ifce.br.iosda.org>
2018-12-07 14:59:53 -0300

    add sort day

--- PomarTests/FeedbackGroup.swift
+++ PomarTests/FeedbackGroup.swift
@@ -7,16 +7,9 @@
 //
 
 import XCTest
+@testable import Pomar
 
 class FeedbackGroup: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testExample() {
         // This is an example of a functional test case.
@@ -29,5 +22,7 @@ class FeedbackGroup: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+
+  
 
 }
